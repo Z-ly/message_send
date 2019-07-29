481eb9a4fc0e621daf03aa4ad799090af310c63d
Z-ly <48954187+Z-ly@users.noreply.github.com>
2019-07-29 10:57:12 +0800

    Update message_send.py

--- message_send.py
+++ message_send.py
@@ -12,8 +12,8 @@ import xlrd
 driver=webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
 driver.get('http://campus.info.bit.edu.cn/wechat/index')
 #a = driver.find_element_by_class_name("btn").click()
-driver.find_element_by_id("username").send_keys('2120170285')
-driver.find_element_by_id("password").send_keys('zly12580')
+driver.find_element_by_id("username").send_keys('0000000')
+driver.find_element_by_id("password").send_keys('0000000')
 driver.find_element_by_class_name("btn_image").click()
 
 #—————————读取短信————————————
